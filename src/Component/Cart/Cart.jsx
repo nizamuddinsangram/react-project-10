5894a968ec0c7338cac4a1fb43db18b1c8f59846
Sangram-Uddin <73923933+Sangram-Uddin@users.noreply.github.com>
2023-03-30 19:42:45 +0600

    first commit

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Cart.css'
 const Cart = (props) => {
-    console.log(props.product)
+    console.log(props)
     const {img,name,seller,quantity,price,ratings}=props.product;
+ 
+    const handleAddToCard=props.handleAddToCard;
+
     return (
         <div className='product'>
         <img src={img}/>
@@ -13,7 +19,9 @@ const Cart = (props) => {
         <p>Rating:{ratings} Stars</p>
         
         </div>
-        <button className='btn-cart'>Add to Card</button>
+        <button onClick={()=>handleAddToCard(props.product)} className='btn-cart'>Add to Card
+        <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
         </div>
     );
 };
--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -3,13 +3,17 @@ import Cart from '../Cart/Cart';
 import './Shop.css'
 const Shop = () => {
   const [products,setProduct]=useState([]);
-
+  const [cart,setCart]=useState([])
 
   useEffect(()=>{
     fetch('products.json')
     .then(res=>res.json())
     .then(data=>setProduct(data))
   },[])
+  const handleAddToCard=(product)=>{
+   const newCart=[...cart,product];
+   setCart(newCart);
+}
     return (
         <div>
             <div className="shop-container">
@@ -19,12 +23,14 @@ const Shop = () => {
                products.map(product=><Cart
                key={product.id}
                product={product}
+               handleAddToCard={handleAddToCard}
                ></Cart>) 
 
             }
             </div>  
              <div className="product-total-impormation">
               <h4>Order summary</h4>
+              <p>Selected item:{cart.length}</p>
              </div>
             </div>
         </div>
